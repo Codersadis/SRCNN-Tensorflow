7664c06cdafafc7845e89bf7bf8449d449403f53
sadis Jia <jcm332@163.com>
2017-02-10 10:54:24 +0800

    Add driver code

---
+++ driver_train.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 10:07:25 2017
+
+@author: cmjia
+"""
+
+import os
+
+os.system("python main.py --epoch 1000 --is_train True") # training code
+# os.system("python main.py --is_train False --is_default_test_img False --img_name bird_GT.bmp") # test code
+
